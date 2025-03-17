abc5a85d6f9af9693eaef6c4905b2bce2c5d167d
melkijo <melkijonathan2@gmail.com>
2025-03-17 11:45:14 +0700

    Initial Commit

---
+++ Academy App One/Academy_App_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Academy_App_OneApp.swift
+//  Academy App One
+//
+//  Created by Melki Jonathan Andara on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Academy_App_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Academy App OneTests/Academy_App_OneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Academy_App_OneTests.swift
+//  Academy App OneTests
+//
+//  Created by Melki Jonathan Andara on 17/03/25.
+//
+
+import Testing
+@testable import Academy_App_One
+
+struct Academy_App_OneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
