2af5f4cb13a49b5afca8adb34428f8c6930c350b
melkijo <jonathanandaraa@gmail.com>
2025-03-26 11:20:35 +0700

    Fix: input field indicator and caption

---
+++ Academy App One/components/HistoryChart.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryChart.swift
+//  Academy App One
+//
+//  Created by Melki Jonathan Andara on 25/03/25.
+//
+
+import SwiftUI
+
+struct HistoryChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryChart()
+}
